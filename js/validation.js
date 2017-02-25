f9cf51a9efb93b387355a96c4ff2fe557ee154e3
Carlos Junod <carlos@junod.cl>
2017-02-24 22:23:19 -0500

    Adding JS to validate form, impoving HTML semantic syntax

--- js/validation.js
+++ js/validation.js
@@ -15,83 +15,113 @@ class InputValidator{
     }
 
     addError(error){
-        this.errors.push(error);
+        var toList = '<li>' + error + '</li>';
+        this.errors.push(toList);
     }
 
     getErrors(){
         var status = this.input.validity;
 
-        if (status.tooLong) {
-            this.addError("the value is too long");
-        }
-
-        if(status.tooShort){
-            this.addError("the value is too short");
-        }
-
-        if (status.typeMismatch) {
-            this.addError("please check your answer");
-        }
-
         if (status.valueMissing) {
             this.addError("this value is required");
+        } else if (this.type == 'tel') {
+            if (this.input.value != '') {
+                if (!this.input.value.match(/((\(\d{3}\) ?)|(\d{3}-))?\d{3}-\d{4}/g)) {
+                    this.addError("this is not a valid phone number");
+                }
+            }
         }
 
-        if (status.patternMismatch){
-            if (!this.input.value.match(/[A-Z]/g)) {
-                this.addError("this should have at least one cappital letter");
-            }
-            if (!this.input.value.match(/[0-9]/g)) {
-                this.addError("this should have at least one number");
-            }
-            if (!this.input.value.match(/[-!$%^&*()_+|~=`{}\[\]:";'<>?,.\/]/g)) {
-                this.addError("this should have at least one symbol");
+        if (this.type == 'text' || this.type == 'password') {
+            if (status.tooLong) {
+                this.addError("the value is too long");
             }
 
+            if(status.tooShort){
+                this.addError("the value is too short");
+            }
         }
 
+        if(this.type == 'password'){
+            if (!this.input.value.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}/g)){
+                if (!this.input.value.match(/[A-Z]/g)) {
+                    this.addError("this should have at least one cappital letter");
+                }
+                if (!this.input.value.match(/[0-9]/g)) {
+                    this.addError("this should have at least one number");
+                }
+                if (!this.input.value.match(/[-!$%^&*()_+|~=`{}\[\]:";'<>?,.\/]/g)) {
+                    this.addError("this should have at least one symbol");
+                }
+            }
+        }
+
+        if(this.type == 'email'){
+            if(!this.input.value.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g)){
+                this.addError("this is not a valid format (eg. name@domain.dot)");
+            }
+        }
     }
-
-
-
 }
 
 
 submit.addEventListener('click', (el)=> {
+
     el.preventDefault();
 
+    console.log("REACHED");
+
+    //Creating objects
     var fullNameValidate = new InputValidator(fullName, "text");
     var emailValidate = new InputValidator(email, "email");
     var passwordValidate = new InputValidator(password, "password");
     var phoneValidate = new InputValidator(phone, "tel");
+    var messages = document.querySelectorAll('.message');
 
+    messages.forEach((messages)=>{
+        if (!messages.classList.contains('alert')) {
+                messages.className += ' alert';
+        }
+
+    })
+
+
+    //messages
+    var msgName = document.querySelector('[data-name]');
+    var msgEmail = document.querySelector('[data-email]');
+    var msgPass = document.querySelector('[data-password]');
+    var msgPhone = document.querySelector('[data-phone]');
+    var msgCountry = document.querySelector('[data-country]');
+
+    //getting te errors
     fullNameValidate.getErrors();
     emailValidate.getErrors();
     passwordValidate.getErrors();
     phoneValidate.getErrors();
 
-    console.log("name Errors:");
-    console.log(fullNameValidate.input.validity);
-    console.log(fullNameValidate.errors);
-    console.log("----------");
+    // printing messages
+    msgName.innerHTML = fullNameValidate.errors.join("");
+    msgEmail.innerHTML = emailValidate.errors.join("");
+    msgPass.innerHTML = passwordValidate.errors.join("");
+    msgPhone.innerHTML = phoneValidate.errors.join("");
+    msgPass.innerHTML = passwordValidate.errors.join("");
 
-    console.log("email Errors:");
-    console.log(emailValidate.input.validity);
-    console.log(emailValidate.errors);
-    console.log("----------");
+    console.log(messages);
 
-    console.log("password Errors:");
-    console.log(passwordValidate.input.validity);
-    console.log(passwordValidate.errors);
-    console.log("----------");
-    console.log("phone Errors:");
-    console.log(phoneValidate.input.validity);
-    console.log(phoneValidate.errors);
-    console.log("----------");
-
-    // fullNameValidate.input.validity.forEach((error)=>{
-    //     fullNameValidate.errors.push(error);
-    //     console.log(error + " into the array");
-    // })
+    if (msgName.innerHTML === "") {
+        console.log("IS EMPTY");
+        submit.removeEventListener('click', this);
+        submit.addEventListener('click', done());
+    }
 
 })
+
+function done(){
+    var form = document.querySelector('#form');
+    form.innerHTML = `
+    <div id="submited">
+    <img src="images/circle-check.svg" alt="ok" />
+    <h2>Awesome, you're registered</h2>
+    </h1>`
+
+}
